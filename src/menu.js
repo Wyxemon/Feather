f8c3cdfb5df8ef2681cf19f95bef124756afc70e
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-03-09 15:50:42 +0100

    Create version list.
    Whe add when you create a version we add to list the version in a UI.

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,7 @@ const newBoxBtn = document.getElementById('new-box-btn');
 const modal = document.getElementById('new-box-modal');
 const closeBtn = document.querySelector('.close-btn');
 const downloadButton = document.querySelector('.download-button');
+const divBox = document.getElementById('div__box');
 
 modal.style.display = 'none';
 
@@ -24,4 +25,16 @@ downloadButton.onclick = function() {
     const memory = document.querySelectorAll('.dropdown')[1].value.replace(' ', '');
     console.log('Download button clicked with version:', version, 'and memory:', memory);
     window.electron.startDownload(version, memory);
+    newBox(version);
+}
+
+function newBox(version) {
+    const divBox = document.getElementById('div__box');
+    const newBoxInstall = document.createElement('div');
+    newBoxInstall.id = 'div__box__install';
+    newBoxInstall.innerHTML = `
+        <img src="resources/box.png">
+        <p>Version ${version}</p>
+    `;
+    divBox.appendChild(newBoxInstall);
 }
