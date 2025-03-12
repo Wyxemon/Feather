7c7afd5e5846d92a4509844bc76fdd4f5086e7a7
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-03-12 19:19:38 +0100

    Simplify the proyect.

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,11 @@
-const newBoxBtn = document.getElementById('new-box-btn');
 const modal = document.getElementById('new-box-modal');
 const closeBtn = document.querySelector('.close-btn');
 const downloadButton = document.querySelector('.download-button');
+const playButton = document.getElementById('button--play');
+const playModalButton = document.querySelector('.play-button');
 const divBox = document.getElementById('div__box');
-const news = document.getElementById('news');
 
 modal.style.display = 'none';
-
-newBoxBtn.onclick = function() {
-    modal.style.display = 'block';
-}
 
 closeBtn.onclick = function() {
     modal.style.display = 'none';
@@ -21,31 +17,21 @@ window.onclick = function(event) {
     }
 }
 
-downloadButton.onclick = function() {
+playButton.onclick = function() {
+    modal.style.display = 'block';
+}
+
+playModalButton.onclick = function() {
     const version = document.querySelector('.dropdown').value;
-    const memory = document.querySelectorAll('.dropdown')[1].value.replace(' ', '');
-    console.log('Download button clicked with version:', version, 'and memory:', memory);
-    
+    const memory = document.querySelectorAll('.dropdown')[1].value.replace(' ', '');    
     // Mostrar el div de carga
     const loadingDiv = document.getElementById('div__loading');
     loadingDiv.style.display = 'flex';
     
-    window.electron.startDownload(version, memory);
-    newBox(version);
+    window.electron.startPlay(version, memory);
     
     // Esconder el div de carga después de 5 segundos
     setTimeout(() => {
         loadingDiv.style.display = 'none';
     }, 15000);
-}
-
-function newBox(version) {
-    const divBox = document.getElementById('div__box');
-    const newBoxInstall = document.createElement('div');
-    newBoxInstall.id = 'div__box__install';
-    newBoxInstall.innerHTML = `
-        <img src="resources/box.png">
-        <p>Version ${version}</p>
-    `;
-    divBox.appendChild(newBoxInstall);
 }
